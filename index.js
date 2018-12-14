ace4310c72836b4a8e205d3cf6fc3b622fae385f
tresor <814908133@qq.com>
2018-12-14 17:46:48 +0800

    加入react ssr

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const Koa = require('koa');
-const app = new Koa();
-const router = require('./router');
-const middleware = require('./middleware');
+require('babel-core/register')()
+require('babel-polyfill')
 
-middleware(app);
-router(app);
-
-app.listen(3000, () => {
-    console.log('server is running at http://localhost:3000')
-})
+require('./server')
